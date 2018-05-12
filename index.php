f40b2a203461de5fa72e99609c15fd1cd5096a1e
lythanhngodev <lythanhngodev@gmail.com>
2018-05-13 06:55:44 +0700

    .

--- index.php
+++ index.php
@@ -12,10 +12,9 @@
 	<base href="<?php echo $qlkh['HOSTGOC'] ?>" />
 	<!-- Facebook SEO -->
 	<meta property="og:url" content="<?php echo $qlkh['HOSTGOC'] ?>" />
-	<meta property="og:title" content="Phòng nghiên cứu khoa học - Đại học Sư phạm kỹ thuật Vĩnh Long" />
 	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Phòng nghiên cứu khoa học - Đại học Sư phạm kỹ thuật Vĩnh Long" />
 	<meta property="og:image" content="<?php echo $qlkh['HOSTGOC'] ?>images/vlute.jpg" />
-
 	<link rel="shortcut icon" href="<?php echo $qlkh['HOSTGOC'] ?>images/favicon.ico" />
 
 	<script defer src="fontawesome/svg-with-js/js/fontawesome-all.js"></script>
