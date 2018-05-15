e6acf463460b9d7a54c7ed789e9c102afd82430e
lythanhngodev <lythanhngodev@gmail.com>
2018-05-15 08:42:21 +0700

    ./
    ./

---
+++ control/c_xem_tin.php
@@ -0,0 +1,4 @@
+<?php
+include_once 'model/m_xem_tin.php';
+include_once 'view/v_xem_tin.php';
+?>
---
+++ view/v_xem_tin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ly Thanh Ngo
+ * Date: 15/05/2018
+ * Time: 8:28 AM
+ */
+?>
+<div class="chi-tiet-bai-viet">
+    <div class="vlu-chi-tiet-header-bai-viet">
+        <article class="vlu-chi-tiet-hinh-anh-tin" style="background-image: url('')">
+            <a href="#">
+                <div class="vlu-chi-tiet-link-bai-viet">
+                    <div class="vlu-chi-tieu-tieu-de-bai-viet">Phát bằng tốt nghiệp cho 173 tân kỹ sư</div>
+                    <div class="vlu-chi-tiet-thoi-gian-bai-viet"><i class="fa fa-clock-o"></i>&nbsp;12-08-017&nbsp;&nbsp;|&nbsp;&nbsp;Lượt xem: 123456</div>
+                </div>
+            </a>
+        </article>
+    </div>
+    <div class="vlu-chi-tiet-bai-viet">
+        Noi dung chi tiet nam o day!
+    </div>
+</div>
