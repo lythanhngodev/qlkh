86406a7fcb2c76abd3615e4758305695620f0f4d
lythanhngodev <lythanhngodev@gmail.com>
2018-05-27 03:16:09 +0700

    u

--- index_mb.php
+++ index_mb.php
@@ -30,7 +30,8 @@
     <img src="images/header_vlute.png" style="width: 100%;" />
   </div>
 </div>
-<nav class="navbar navbar-lg navbar-light bg-light">
+<div class="container">
+<nav class="navbar navbar-lg navbar-light">
   <a class="navbar-brand" href="#">Menu</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -102,5 +103,6 @@
     </ul>
   </div>
 </nav>
+</div>
 </body>
 </html>
