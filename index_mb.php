44c4003b7dec81fb5d90237871d8fa7f60c4ea6e
lythanhngodev <lythanhngodev@gmail.com>
2018-05-27 01:49:58 +0700

    i

--- index_mb.php
+++ index_mb.php
@@ -2,15 +2,15 @@
 <html amp lang="en">
   <head>
     <meta charset="utf-8">
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
     <title>Nghiên cứu khoa học &amp; Hợp tác quốc tế | Đại học Sư phạm Kỹ thuật Vĩnh Long</title>
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 <style amp-custom>
 body{background:#000; color:#fff;}
 </style>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
   </head>
-  <body>
-    <h1>Welcome to the mobile web</h1>
-  </body>
-  </html>
+<body>
+	<h1>Welcome to the mobile web</h1>
+</body>
+</html>
