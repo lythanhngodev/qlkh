22d0b3212977e15df49b19f9e40d42ff5431a2b9
lythanhngodev <lythanhngodev@gmail.com>
2018-05-26 20:10:56 +0700

    ./

--- header/h_trang_chu.php
+++ header/h_trang_chu.php
@@ -4,7 +4,7 @@
 <!-- Facebook SEO -->
 <meta property="og:url" content="<?php echo "http://". $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" />
 <meta property="og:type" content="article" />
-<meta property="og:title" content='Phòng nghiên cứu khoa học | Đại học Sư phạm kỹ thuật Vĩnh Long' />
+<meta property="og:title" content='Phòng nghiên cứu khoa học &amp; Hợp tác quốc tế | Đại học Sư phạm Kỹ thuật Vĩnh Long' />
 <meta property="og:image" content="<?php echo $qlkh['HOSTGOC'] ?>images/vlute-o.jpg" />
 <meta property="fb:app_id" content="2165745763451934" /> 
 <link rel="shortcut icon" href="<?php echo $qlkh['HOSTGOC'] ?>images/favicon.ico" />
