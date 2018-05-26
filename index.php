bc081cbeeb43896b78e6ffff10d0269277ec5604
lythanhngodev <lythanhngodev@gmail.com>
2018-05-27 03:23:13 +0700

    g

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   require_once($ZERO_PATH);
   $detect = new Mobile_Detect;
   if($detect->isMobile()){
-  	require_once('index_mb.php'); // Giao diện trên di động
+  	//require_once('index_mb.php'); // Giao diện trên di động
+    require_once('index_pc.php'); // Giao diện trên PC
   }
   else
   {
