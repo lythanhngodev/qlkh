d60ce04d94732a5d1b88fdc39b68ceed56f92ea0
lythanhngodev <lythanhngodev@gmail.com>
2018-05-31 16:05:25 +0700

    Update index_mb.php

--- index_mb.php
+++ index_mb.php
@@ -124,6 +124,17 @@
             }, 400);
             return false;
         });
+	$(window).bind('orientationchange resize', function(event){
+	  if (event.orientation) {
+	    if (event.orientation == 'landscape') {
+	      if (window.rotation == 90) {
+		rotate(this, -90);
+	      } else {
+		rotate(this, 90);
+	      }
+	    }
+	  }
+	});
 });
 </script>
 </html>
