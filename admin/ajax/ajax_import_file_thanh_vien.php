3a42dfd99f7b72e337205e4903afff282a7bddc6
lythanhngodev <lythanhngodev@gmail.com>
2018-06-07 23:34:28 +0700

    ex

--- admin/ajax/ajax_import_file_thanh_vien.php
+++ admin/ajax/ajax_import_file_thanh_vien.php
@@ -15,10 +15,7 @@ if (isset($_SESSION['tdn']) && isset($_SESSION['pas'])) {
 require_once "../excel/PHPExcel.php";
 $file = $_FILES['file']['tmp_name'];
 $objReader = PHPExcel_IOFactory::createReaderForFile($file);
-$listWorkSheets = $objReader->listWorksheetNames($file);
 $danhsach = null;
-for($i=0;$i<count($listWorkSheets);$i++){
-    $objReader->setLoadSheetsOnly($listWorkSheets[$i]);
     $objExcel = $objReader->load($file);
     $sheetData = $objExcel->getActiveSheet()->toArray('null',true,true,true);
     $hightsRow = $objExcel->setActiveSheetIndex()->getHighestRow();
@@ -32,7 +29,7 @@ for($i=0;$i<count($listWorkSheets);$i++){
         $t_ds = [$ho,$ten,$mail];
         $danhsach[] = $t_ds;
     }
-}
+
 ?>
 <table id="bangthanhvien" class="table table-bordered table-hover">
     <thead>
