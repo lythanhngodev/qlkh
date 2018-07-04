e5b35c82a5c0675a82b22b8e6fd71ad581f70752
lythanhngodev <lythanhngodev@gmail.com>
2018-07-04 20:39:14 +0700

    Speed

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  ob_start("ob_gzhandler");
+ //if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {ob_start('ob_gzhandler');}
   require_once("_m__detect.php");
   $detect = null;
   $detect = new Mobile_Detect();
