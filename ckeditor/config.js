9897635ba09e09e9d05c084b69e4e45f87d66a06
lythanhngodev <lythanhngodev@gmail.com>
2018-07-09 09:09:00 +0700

    Speed

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	//config.language = 'vi';
+	config.language = 'vi';
 	config.uiColor = '#ecf0f1';
 	config.extraPlugins = 'youtube';
 	config.extraPlugins = 'slideshow';
